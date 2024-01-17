7916b64ebb32bec360e445723b4608e78dd5d563
blancaflx <96093777+blancaflx@users.noreply.github.com>
2024-01-17 14:42:00 -0700

    Agregando else y comentarios
    Parte del apartado de Condiciones y concatenación del curso Lógica de programación: sumérgete en la programación con JavaScript

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-
-
-let numeroScreto = 6;
-let numeroUsuario= prompt("DIME UN NUMERO AAAA");
+//Variables
+let numeroSecreto = 2;
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
-
-if (numeroUsuario == numeroScreto){
-    alert('Ese es :D');
+/*
+Este código realiza
+la comparación
+*/
+if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condición
+    alert(`Acertaste, el número es: ${numeroUsuario}`);
+} else {
+    //La condición no se cumplió
+    alert('Lo siento, no acertaste el número');
 }
 
