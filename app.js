ecd04d326ee4aa949c58d1b3da977d9cd2b163a7
blancaflx <96093777+blancaflx@users.noreply.github.com>
2024-01-19 12:30:15 -0700

    Condiciones y concatenacion
    Cambios del curso Logica de programacion: sumergete en la programación con JS

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //Variables
-let numeroSecreto = 2;
+let numeroSecreto = 8;
 let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
+console.log("numero usuario "+numeroUsuario);
+console.log("numero secreto "+numeroSecreto);
 /*
 Este código realiza
 la comparación
@@ -13,5 +14,6 @@ if (numeroUsuario == numeroSecreto) {
 } else {
     //La condición no se cumplió
     alert('Lo siento, no acertaste el número');
+    
 }
 
