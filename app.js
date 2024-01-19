7deb429ed7d6fb2a88605c99054faa677f62834c
blancaflx <96093777+blancaflx@users.noreply.github.com>
2024-01-19 13:16:36 -0700

    Loops y tentativas
    Cambios del curso Lógica de programación: sumérgete en la programación con JS

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 //Variables
-let numeroSecreto = 8;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+let numeroSecreto = 4;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'intento';
 
-console.log("numero usuario "+numeroUsuario);
-console.log("numero secreto "+numeroSecreto);
-/*
-Este código realiza
-la comparación
-*/
-if (numeroUsuario == numeroSecreto) {
-    //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    //La condición no se cumplió
-    alert('Lo siento, no acertaste el número');
-    
+while(numeroUsuario != numeroSecreto){
+
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+
+    console.log("numero usuario "+numeroUsuario);
+    console.log("numero secreto "+numeroSecreto);
+    /*
+    Este código realiza
+    la comparación
+    */
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+    } else {
+        if(numeroUsuario > numeroSecreto){
+            alert('El numero secreto es menor');
+        }
+        else{
+            alert('El numero secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        intentos = intentos + 1;
+        palabraVeces= 'intentos';
+        //La condición no se cumplió
+    // alert('Lo siento, no acertaste el número');
+        
+    }
+
 }
+
 
